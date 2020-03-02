2dc34b00aba09fd8ce5e76eebd64afdc8de95da5
Ali Hassan <iali.hassan@icloud.com>
2020-03-02 23:28:50 +0500

    Fixed Post Edit in Dashboard

--- resources/views/pages/dashboard/posts/edit.blade.php
+++ resources/views/pages/dashboard/posts/edit.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+<?php
+$str = $post->body;
+$str = preg_replace_callback(
+    '|(?s)(?<=<code>).*?(?=</code>)|',
+    function ($matches) {
+        return str_replace('<', '&lt;', str_replace('>', '&gt;', $matches[0]));
+    },
+    $str
+);
+$str = preg_replace_callback(
+    '|(?s)(?<=<pre class="prettyprint">).*?(?=</pre>)|',
+    function ($matches) {
+        return str_replace('<', '&lt;', str_replace('>', '&gt;', $matches[0]));
+    },
+    $str
+);
+?>
 <div class="container">
     <div class="row">
+        {{ $str }}
         <div class="col-md-10">
             {!! Form::open(['action' => ['PostController@update', $post->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
                 <div class="form-group">
@@ -11,7 +29,7 @@
                 </div>
                 <div class="form-group">
                     {{ Form::label('body', 'Body') }}
-                    {{ Form::textarea('body', $post->body, ['class' => 'form-control', 'id' => 'article-ckeditor'])  }}
+                    {{ Form::textarea('body', htmlentities($str), ['class' => 'form-control', 'id' => 'article-ckeditor'])  }}
                 </div>
                 <div class="form-group">
                     {{ Form::label('categories', 'Categories') }}
